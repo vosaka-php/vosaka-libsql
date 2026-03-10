199291debfd566977ec7ef87ea1d081115db77b9
VennDev <1050080275@sv.hcmunre.edu.vn>
2026-03-11 00:28:34 +0700

    Update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'venndev/vosaka-libsql',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0cb03b70a6c0e392fda3576969b7518b61baa09b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -38,9 +38,9 @@
             'dev_requirement' => false,
         ),
         'venndev/vosaka-libsql' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0cb03b70a6c0e392fda3576969b7518b61baa09b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
